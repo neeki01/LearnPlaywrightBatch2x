c91d77210e0ba587d82cbb8e41e1f91ca8ed7983
neeki01 <kumarineeki321@gmail.com>
2026-06-20 19:29:17 +0530

    Add chapter 19 Playwright config, tests, CI workflow and update chapter 16/18 examples

---
+++ chapter_19_Playwright_Basics/tests/codegen_tta-cart_spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test( 'test', async ( { page } ) => {
+    await page.goto( 'https://app.thetestingacademy.com/playwright/ttacart/' );
+    await page.locator( '[data-test="username"]' ).click();
+    await page.locator( '[data-test="username"]' ).fill( 'abc' );
+    await page.locator( '[data-test="password"]' ).click();
+    await page.locator( '[data-test="password"]' ).fill( 'dgdw235' );
+    await page.locator( '[data-test="login-button"]' ).click();
+    await expect( page.locator( '[data-test="login-button"]' ) ).toBeVisible();
+    await expect( page.locator( '[data-test="error"]' ) ).toContainText( 'Epic sadface: Username and password do not match any user in this service' );
+    await expect( page.locator( '[data-test="login-container"]' ) ).toMatchAriaSnapshot( `
+    - textbox "Username": abc
+    - textbox "Password": dgdw235
+    - alert: "Epic sadface: Username and password do not match any user in this service"
+    - button "Login"
+    `);
+} );
